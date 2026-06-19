12ef9dcd10930fd163377f560012937ea1eccf99
Aftab Zaaman <aftabzaaman@example.com>
2026-06-19 15:55:15 +0600

    db: configure postgresAdapter push: true to automatically synchronize schema in production

--- apps/web/src/payload.config.ts
+++ apps/web/src/payload.config.ts
@@ -30,6 +30,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URL || "",
     },
+    push: true,
   }),
   typescript: {
     outputFile: path.resolve(dirname, "payload-types.ts"),
